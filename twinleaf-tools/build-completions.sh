7376cd79ef7e49249e48b94da820cfab3df86491
rasen68 <rasenkyuchotatairenranshuriken@gmail.com>
2026-07-01 18:35:03 -0400

    Feat (completions): build-completions.sh to maintain completion-scripts

---
+++ twinleaf-tools/build-completions.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+echo "Initial build..."
+cargo build
+TIO=../target/debug/tio
+COMP=completion-scripts/tio_completions
+
+# generate static scripts
+echo "Generating static scripts"
+"$TIO" completions --static bash > "$COMP"_static.bash
+"$TIO" completions --static zsh > "$COMP"_static.zsh
+echo "Re-build..."
+cargo build # build again to embed static scripts
+
+# generate dynamic scripts
+echo "Generating dynamic scripts"
+"$TIO" completions bash > "$COMP"_dynamic.bash
+"$TIO" completions zsh > "$COMP"_dynamic.zsh
+
+echo "Testing generated scripts"
+bash -n "$COMP"_dynamic.bash || exit
+zsh -n "$COMP"_dynamic.zsh || exit
+
+if [[ "$1" == "--commit" ]]; then
+	echo "Creating git commit"
+	git add "$COMP"*
+	git commit -m "Chore: Regenerate completion scripts"
+fi
