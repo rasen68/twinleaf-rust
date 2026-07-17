ab31c8acbf5e65ba10fa8e74a29695865fe953bb
rasen68 <rasenkyuchotatairenranshuriken@gmail.com>
2026-07-17 01:45:38 -0400

    CI: check completions are working and committed in github workflows

--- twinleaf-tools/build-completions.sh
+++ twinleaf-tools/build-completions.sh
@@ -8,19 +8,19 @@ COMP=completion-scripts/tio_completions
 
 # generate static scripts
 echo "Generating static scripts"
-"$TIO" completions --static bash > "$COMP"_static.bash
-"$TIO" completions --static zsh > "$COMP"_static.zsh
+"$TIO" completions --static bash > "$COMP"_static.bash || exit 1
+"$TIO" completions --static zsh > "$COMP"_static.zsh || exit 1
 echo "Re-build..."
 cargo build # build again to embed static scripts
 
 # generate dynamic scripts
 echo "Generating dynamic scripts"
-"$TIO" completions bash > "$COMP"_dynamic.bash
-"$TIO" completions zsh > "$COMP"_dynamic.zsh
+"$TIO" completions bash > "$COMP"_dynamic.bash || exit 1
+"$TIO" completions zsh > "$COMP"_dynamic.zsh || exit 1
 
 echo "Testing generated scripts"
-bash -n "$COMP"_dynamic.bash || exit
-zsh -n "$COMP"_dynamic.zsh || exit
+bash -n "$COMP"_dynamic.bash || exit 1
+zsh -n "$COMP"_dynamic.zsh || exit 1
 
 if [[ "$1" == "--commit" ]]; then
 	echo "Creating git commit"
